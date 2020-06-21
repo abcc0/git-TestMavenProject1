453c0eeef3f38c924b0b3352454834578d9978f9
jv0408 <jv0408@outlook.com>
2020-06-21 18:25:08 -0400

    First commit

---
+++ src/main/java/com/abcc/maven/jmath/NumberAdditions.java
@@ -0,0 +1,19 @@
+package com.abcc.maven.jmath;
+
+public class NumberAdditions {
+
+	public int add2numbers (int iNum1, int iNum2) {
+	
+		int	iResult = iNum1 + iNum2;
+				
+	return iResult;
+	}
+	
+	public int add3numbers (int iNum1, int iNum2, int iNum3) {
+		
+		int	iResult = iNum1 + iNum2 + iNum3;
+				
+	return iResult;
+	}
+
+}
---
+++ src/test/java/com/abcc/maven/jmath/NumberAdditionTest.java
@@ -0,0 +1,80 @@
+package com.abcc.maven.jmath;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class NumberAdditionTest {
+
+	@Test
+	public void A2N_TestCase1() {
+	
+		NumberAdditions TestNA;
+		
+		TestNA = new NumberAdditions();
+		int iResult = TestNA.add2numbers(15, 25);
+		assertEquals(iResult, 40);
+	}
+	
+	@Test
+	public void A2N_TestCase2() {
+	
+		NumberAdditions TestNA;
+		
+		TestNA = new NumberAdditions();
+		int iResult = TestNA.add2numbers(30, 25);
+		assertEquals(iResult, 55);
+	}
+	
+	@Ignore
+	@Test
+	public void A2N_TestCase3() {
+	
+		NumberAdditions TestNA;
+		
+		TestNA = new NumberAdditions();
+		int iResult = TestNA.add2numbers(75, 25);
+		assertEquals(iResult, 100);
+	}
+	
+	@Test
+	public void A3N_TestCase1() {
+	
+		NumberAdditions TestNA;
+		
+		TestNA = new NumberAdditions();
+		int iResult = TestNA.add3numbers(15, 25, 30);
+		assertEquals(iResult, 70);
+	}
+	
+	@Ignore
+	@Test
+	public void A3N_TestCase2() {
+	
+		NumberAdditions TestNA;
+		
+		TestNA = new NumberAdditions();
+		int iResult = TestNA.add3numbers(15, 25, 30);
+		assertEquals(iResult, 70);
+	}
+	
+	@Test
+	public void A3N_TestCase3() {
+	
+		NumberAdditions TestNA;
+		
+		TestNA = new NumberAdditions();
+		int iResult = TestNA.add3numbers(80, 20, 30);
+		assertEquals(iResult, 130);
+	}
+	
+	@Test
+	public void A3N_TestCase4() {
+	
+		NumberAdditions TestNA;
+		
+		TestNA = new NumberAdditions();
+		int iResult = TestNA.add3numbers(10, 20, 30);
+		assertEquals(iResult, 60);
+	}
+}
